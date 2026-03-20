76cc56db5200b63e04eba767f2093a3e404e2d94
Faiz Lawan <139010476+hackerbotfz@users.noreply.github.com>
2026-03-20 01:55:39 +0000

    Add setup.py for NTU Robot Simulation package
    Initial setup configuration for the NTU Robot Simulation package.

---
+++ ntu_robotsim/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+package_name = 'ntu_robotsim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=['pkg'],
+    package_dir={'pkg': 'pkg'},
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ntu-user',
+    maintainer_email='ntu-user@todo.todo',
+    description='NTU Robot Simulation',
+    license='TODO',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'visual_odometry = pkg.visual_odometry:main',
+        ],
+    },
+)
