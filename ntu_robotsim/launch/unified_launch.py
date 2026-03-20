d31d7d23d72a394264e51d3704d3e0734589ff29
Faiz Lawan <139010476+hackerbotfz@users.noreply.github.com>
2026-03-20 09:29:39 +0000

    Add additional nodes and parameters to launch file

--- ntu_robotsim/launch/unified_launch.py
+++ ntu_robotsim/launch/unified_launch.py
@@ -7,53 +7,79 @@ from launch.actions import DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
 import os
 
+
 def generate_launch_description():
 	ntu_sim_dir = get_package_share_directory('ntu_robotsim')
 	odom_tf_dir = get_package_share_directory('odom_to_tf_ros2')
-
 	maze = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
 			os.path.join(ntu_sim_dir, 'launch', 'cwmaze.launch.py')
 		)
 	)
+	
 	atlas = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
 			os.path.join(ntu_sim_dir, 'launch', 'single_robot_sim.launch.py')
 		)
 	)
+	
 	odom_to_tf = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
 			os.path.join(odom_tf_dir, 'launch', 'atlas_odom_to_tf.launch.py')
 		)
 	)
+
+	vo_vo_map_transformer = Node(
+		package = "tf2_ros",
+		executable = "static_transform_publisher",
+		arguments = ["0", "0", "0", "0", "0", "0", "map", "vo_odom"]
+	)
+	
+	map_transformer = Node(
+		package = "tf2_ros",
+		executable = "static_transform_publisher",
+		arguments = ["0", "0", "0", "0", "0", "0", "map", "vo_odom"]
+	)
+	
+	vo_map_transformer = Node(
+		package = "tf2_ros",
+		executable = "static_transform_publisher",
+		arguments = ["0", "0", "0", "0", "0", "0", "map", "vo_odom"]
+	)
+	
 	map_transformer = Node(
 		package = "tf2_ros",
 		executable = "static_transform_publisher",
 		arguments = ["0", "0", "0", "0", "0", "0", "map", "odom"]
 	)
+	
 	octomap = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
 			os.path.join(ntu_sim_dir, 'launch', 'octomap.launch.py')
 		)
 	)
+	
 	nav2 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
 			os.path.join(ntu_sim_dir, 'launch', 'nav2.launch.py')
 		)
 	)
+	
 	rviz = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
 			os.path.join(ntu_sim_dir, 'launch', 'rviz.launch.py')
 		)
 	)
+
 	visual_odometry = Node(
 		package = "ntu_robotsim",
 		executable = "visual_odometry.py",
 		name = "visual_odometry",
 		prefix='xterm -e',
 		output = "screen",
-		parameters=[{'odom_frame': 'vo_odom'}]
+		parameters=[{'odom_frame': 'vo_odom', 'base_frame': 'atlas/vo_base_link'}]
 	)
+
 	teleop = Node(
 		package='teleop_twist_keyboard',
 		executable='teleop_twist_keyboard',
@@ -62,6 +88,7 @@ def generate_launch_description():
 		output='screen',
 		remappings=[('cmd_vel', 'atlas/cmd_vel')]
 	)
+
 	landmark_database = Node(
     	package='ntu_robotsim',
     	executable='landmark_database.py',
@@ -79,10 +106,12 @@ def generate_launch_description():
 		atlas,
 		odom_to_tf,
 		map_transformer,
+		vo_map_transformer,
 		octomap,
 		nav2,
 		rviz,
 		teleop,
 		visual_odometry,
 		landmark_database,
+
 	])
